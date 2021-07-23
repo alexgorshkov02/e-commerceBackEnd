af79f58fa6faecdb4d42a421a77ce705a7e6a523
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-07-23 00:35:13 -0400

    Fixed the issue when the app was run twice

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const routes = require("./routes");
-const sequelize = require('./config/connection');
-
+const sequelize = require("./config/connection")
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  sequelize.sync({ force: true }).then(() => {
-    console.log(`App listening on port ${PORT}!`);
-  });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
